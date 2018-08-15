537cbe97ecb94d4c3ffded5aae37a19a47784618
Deven Yang <394707312@qq.com>
2018-08-15 16:00:21 +0800

    initial version

---
+++ 1.Get-LocalAdminUserInfo.ps1
@@ -0,0 +1,42 @@
+﻿ 
+Param( 
+    [Parameter(Mandatory=$True)] 
+    [String]$InputFile,
+
+    [Parameter(Mandatory=$True)] 
+    [String]$AccountName
+) 
+
+$Computers = Import-Csv -Path .\Computers.csv
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+foreach ($Computer in $Computers) {
+    $HostName = $Computer.HostName
+
+    try {
+        $ctype = [System.DirectoryServices.AccountManagement.ContextType]::Machine
+        $context = New-Object -TypeName System.DirectoryServices.AccountManagement.PrincipalContext -ArgumentList $ctype, $HostName
+        $idtype = [System.DirectoryServices.AccountManagement.IdentityType]::SamAccountName
+        $group = [System.DirectoryServices.AccountManagement.GroupPrincipal]::FindByIdentity($context, $idtype, 'Administrators')
+        $user = $group.Members |select * | ? {($_.Name -eq $AccountName) -and ($_.ContextType -eq "Machine")}
+        
+        if ($user -ne $null){
+            Write-Host "Acount Exists in Admin group on $HostName" -ForegroundColor Green
+            Add-Content .\Account_Exists.log -Value "Account exists in Admin group on $HostName"
+            if ($user.PasswordNeverExpires -eq $true) {
+                Write-Host "Password Never Expire on $HostName" -ForegroundColor Green
+                Add-Content .\Password_NeverExpire.log -Value "Account Password Never Expire on $HostName"
+            }else {
+                Write-Host "Password Expires on $HostName" -ForegroundColor Red
+                Add-Content .\Password_Expires.log -Value "Account Password Expires on $HostName"
+            }
+        }else{
+            Write-Host "Acount not exists in Admin group on $HostName" -ForegroundColor Red
+            Add-Content .\Account_NotExists.log -Value "Acount not exists in Admin group on $HostName"
+        }
+        } catch {
+            Write-Host "Connection to $HostName failed" -ForegroundColor Red
+            Add-Content .\Connection_Failed.log -Value "Connection to $HostName failed, detailed failure reason is: $_.Exception.Message"
+        }
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AccountManagement-PowerShell
 This is the script example working with System.DirectoryServices.AccountManagement in PowerShell
+
+
+System.DirectoryServices.AccountManagement provides uniform access and manipulation of user, computer, and group security principals across the multiple principal stores: Active Directory Domain Services (AD DS), active Directory Lightweight Directory Services (AD LDS), and Machine SAM (MSAM). 
+
+In 1.Get-LocalAdminUserInfo.ps1 we domonstrated how to verify if a particular local user account is a member of administrators group of a list of remote servers.
+
+Usage: .\Get-LocalAdminUserInfo.ps1 -InputFile .\Computers.csv -AccountName someuser
+Remeber to run the script with an administrative account which should have admin access to remote servers.
